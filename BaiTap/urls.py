cb252500fff6d678ab9dbdcbdce2fb10963700d2
cofdness <vtkp2002dn@gmail.com>
2016-03-07 20:48:08 +0700

    Added view and template for detailed blog ....

--- BaiTap/urls.py
+++ BaiTap/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-
+from Blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
+
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from .form import PostForm
+from django.shortcuts import redirect
+
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'Blog/post_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
